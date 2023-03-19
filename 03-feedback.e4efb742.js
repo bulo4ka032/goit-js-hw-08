ea3b1cb2e5f01ace515a5bc6fa98871106912f36
bulo4ka032 <122801694+bulo4ka032@users.noreply.github.com>
2023-03-19 19:07:07 +0000

    Deploying to gh-pages from @ bulo4ka032/goit-js-hw-08@5fdf0d9800c3cc13748ed17eea04418323f11743 🚀

--- 03-feedback.e4efb742.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".feedback-form"),t=e.message,a=e.email;e.addEventListener("submit",(function(){evt.preventDefault(),evt.currentTarget.reset(),localStorage.removeItem("feedback-form-state")})),e.addEventListener("input",(function(){const e={email:a.value,message:t.value};localStorage.setItem("feedback-form-state",JSON.stringify(e))})),function(e){const o=JSON.parse(localStorage.getItem("feedback-form-state"));o&&(a.value=o.email,t.value=o.message),console.log(o)}();
-//# sourceMappingURL=03-feedback.e4efb742.js.map
